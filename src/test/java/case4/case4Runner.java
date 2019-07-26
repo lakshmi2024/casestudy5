58f4a75aae071d3b702483a25b2e5d0ca20a31ab
lakshmi2024 <41136610+lakshmi2024@users.noreply.github.com>
2019-07-26 11:04:37 +0530

    cucumber

--- src/test/java/case4/case4Runner.java
+++ src/test/java/case4/case4Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/case4/case4.feature")
+@CucumberOptions(features="src/test/resources/case4/case4.feature",plugin="json:target\\jsonreport.json")
 public class case4Runner {
 
 }
